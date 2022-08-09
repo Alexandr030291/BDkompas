8236fb32141e507c21f15cff59119801ecdd04a5
Alexandr <tFotDA_030291>
2022-08-09 20:39:09 +0300

    Add files via upload

--- DataBase.py
+++ DataBase.py
@@ -1,4 +1,5 @@
 import sqlite3
+import hashlib
 
 
 class Database:
@@ -13,46 +14,89 @@ class Database:
     def __create_table_database(self):
         commands = ["""
             CREATE TABLE IF NOT EXISTS 'docs'(           
-                'id' INT PRIMARY KEY,
+                'id' INTEGER PRIMARY KEY AUTOINCREMENT,
                 'designation' TEXT ,
                 'name' TEXT,
                 'A0' INT, 'A1' INT, 'A2' INT, 'A3' INT, 'A4' INT, 'A5' INT,
+                'file' INT,
                 UNIQUE ('designation')
             );
         """, """
+            CREATE TABLE IF NOT EXISTS 'files'(           
+                'id' INTEGER PRIMARY KEY AUTOINCREMENT,
+                'path' TEXT,
+                'hash' TEXT,
+                UNIQUE ('path')
+                );
+         """, """
             CREATE TABLE IF NOT EXISTS 'items'(           
-                'id' INT PRIMARY KEY,
-                'designation' TEXT ,
-                'name' TEXT,
-                'billet' INT,
-                'billet_type' INT,
+                'id' INTEGER PRIMARY KEY AUTOINCREMENT,
+                'designation' TEXT NOT NULL,
+                'name' TEXT NOT NULL,
+                'billet' INT DEFAULT 0,
+                'billet_type' INT DEFAULT 0,
                 UNIQUE ('designation')
                 );
          """, """
          CREATE TABLE IF NOT EXISTS 'materials'(           
-                'id' INT PRIMARY KEY,
-                'name' TEXT,
-                UNIQUE ('name')
-                );
-         """, """
-         CREATE TABLE IF NOT EXISTS 'billet_type'(           
-                'id' INT PRIMARY KEY,
+                'id' INTEGER PRIMARY KEY AUTOINCREMENT,
                 'name' TEXT,
                 UNIQUE ('name')
                 );
          """, """
          CREATE TABLE IF NOT EXISTS 'products'(           
-                'id' INT PRIMARY KEY,
+                'id' INTEGER PRIMARY KEY AUTOINCREMENT,
                 'name' TEXT,
+                'type' CHAR,
                 UNIQUE ('name')
                 );
          """, """
-         CREATE TABLE IF NOT EXISTS 'products'(           
-                'id' INT PRIMARY KEY,
-                'name' TEXT,
-                UNIQUE ('name')
+         CREATE TABLE IF NOT EXISTS 'link_item_to_item'(           
+                'main' INT ,
+                'dependent' INT ,
+                'count' FLOAT,
+                CONSTRAINT 'link' PRIMARY KEY ('main', 'dependent')
                 );
-         """]
+         """, """
+         CREATE TABLE IF NOT EXISTS 'link_item_to_docs'(           
+                'main' INT ,
+                'dependent' INT ,
+                CONSTRAINT 'link' PRIMARY KEY ('main', 'dependent')
+                );
+         """, """
+         CREATE TABLE IF NOT EXISTS 'link_item_to_materials'(           
+                'main' INT,
+                'dependent' INT,
+                'count' FLOAT,
+                CONSTRAINT 'link' PRIMARY KEY ('main', 'dependent')
+                );
+         """, """
+         CREATE TABLE IF NOT EXISTS 'link_item_to_materials'(           
+                'main' INT,
+                'dependent' INT,
+                'count' FLOAT,
+                CONSTRAINT 'link' PRIMARY KEY ('main', 'products')
+                );
+         """
+                    ]
+        self.cur.execute("""SELECT name FROM sqlite_master WHERE type='table' AND name='billet_type'""")
+        if len(self.cur.fetchall()) == 0:
+            commands += ["""
+                     CREATE TABLE 'billet_type'(           
+                            'id' INTEGER PRIMARY KEY AUTOINCREMENT,
+                            'name' TEXT,
+                            UNIQUE ('name')
+                            );
+                     """, """
+                     INSERT INTO 'billet_type' ('id', 'name') VALUES(0, 'Нет');
+                     """, """
+                     INSERT INTO 'billet_type' ('id', 'name') VALUES(1, 'Материал');
+                     """, """
+                     INSERT INTO 'billet_type' ('id', 'name') VALUES(2, 'Собственное изделие');
+                     """, """
+                     INSERT INTO 'billet_type' ('id', 'name') VALUES(3, 'Покупное изделие');
+                     """
+                         ]
         for command in commands:
             self.cur.execute(command)
             self.conn.commit()
